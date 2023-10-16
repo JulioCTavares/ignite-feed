d7895cdf4b27fa7c7b6c908b0c094cbe07f843a0
Julio Tavares <juliotavares197@gmail.com>
2023-10-16 20:43:55 -0300

    fix: add main on App component

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,13 @@ function App() {
       <Header />
      <div className={styles.wrapper}>
       <Sidebar />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <main>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </main>
+     
      </div>
     </>
   )
