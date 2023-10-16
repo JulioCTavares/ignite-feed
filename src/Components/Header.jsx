da842240985230b8e2175ecb8bdab6bc4c563f12
Julio Tavares <juliotavares197@gmail.com>
2023-10-15 21:02:42 -0300

    feat: header compoment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
 
+import igniteLogo from "./../Assets/Ignite simbol.svg";
+
 const Header = () => {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo ignite" />
     </header>
   )
 }
